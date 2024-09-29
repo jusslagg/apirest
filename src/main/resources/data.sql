2959d9f79cf51374df61eb2458f3e91d25d6d2d7
Jesus Gil <lagg312@gmail.com>
2024-09-29 15:58:34 -0300

    modificaciones del data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO factura (fechaEmision, clienteNombre, DNI, total, impuestos) VALUES
 ('2024-01-02', 'Maria Gonzalez', '23456789', 2000.00, 200.00),
 ('2024-01-03', 'Yamila Gonzalez', '34567890', 2500.00, 250.00),
 ('2024-01-04', 'Juan Perez', '33456789', 1800.00, 180.00),
-('2024-01-05', 'Laura Fernandez', '45678901', 2200.00, 220.00);
+('2024-01-05', 'Diego Baz', '45678901', 2200.00, 220.00);
